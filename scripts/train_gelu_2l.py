3321d28355eabd272b4aaf6d28a028eee18b485d
Philippe Chlenski <pac@cs.columbia.edu>
2024-02-19 04:44:42 +0000

    Automate moving directories

--- scripts/train_gelu_2l.py
+++ scripts/train_gelu_2l.py
@@ -31,11 +31,14 @@ base_cfg = {
     "dataset_path": "Skylion007/openwebtext",
     "is_dataset_tokenized": False,
     # SAE Parameters
-    "expansion_factor": 32,
-    "b_dec_init_method": "geometric_median",
+    # "expansion_factor": 32,
+    # "b_dec_init_method": "geometric_median",
+    "expansion_factor": 64,
+    "b_dec_init_method": "mean",
     # Training Parameters
     "lr": 0.0004,
-    "l1_coefficient": 0.00008,
+    # "l1_coefficient": 0.00008,
+    "l1_coefficient": 0.00012,
     "lr_scheduler_name": "constantwithwarmup",
     "train_batch_size": 4096,
     "context_size": 128,
@@ -48,10 +51,11 @@ base_cfg = {
     "use_ghost_grads": True,
     "feature_sampling_window": 1000,
     "dead_feature_window": 5000,
-    "dead_feature_threshold": 1e-6,
+    # "dead_feature_threshold": 1e-6,
+    "dead_feature_threshold": 1e-8,
     # WANDB
     "log_to_wandb": True,
-    "wandb_project": "gelu2l_transcoder_training",
+    "wandb_project": "gelu2l_transcoder_training_2",
     "wandb_entity": "chlenski",
     "wandb_log_frequency": 100,
     # Misc
@@ -60,23 +64,41 @@ base_cfg = {
     "dtype": torch.float32,
 }
 
+
+def move_last_checkpoint(new_name):
+    # Move last checkpoint to appropriate folder
+    files = os.listdir("checkpoints")
+    paths = [os.path.join("checkpoints", basename) for basename in files]
+    most_recent_dir = max(paths, key=os.path.getctime)
+    os.rename(most_recent_dir, new_name)
+    print(f"Moved checkpoint from {most_recent_dir} to {new_name}")
+
+
 for seed in [42, 43]:
-    cfg = base_cfg.copy()
-    cfg["seed"] = seed
+    for layer in [0, 1]:
+        cfg = base_cfg.copy()
+        cfg["seed"] = seed
+        cfg["hook_point"] = f"blocks.{layer}.ln2.hook_normalized"
+        cfg["out_hook_point"] = f"blocks.{layer}.hook_mlp_out"
+        cfg["hook_point_layer"] = layer
+        cfg["out_hook_point_layer"] = layer
 
-    # train transcoder
-    transcoder = language_model_sae_runner(LanguageModelSAERunnerConfig(**cfg))
+        # train transcoder
+        transcoder = language_model_sae_runner(LanguageModelSAERunnerConfig(**cfg))
+        move_last_checkpoint(f"checkpoints/64x_gelu_2l_{layer}/seed{seed}_transcoder")
 
-    # train SAE on hook_point
-    cfg = base_cfg.copy()
-    cfg["seed"] = seed
-    cfg["is_transcoder"] = False
-    sparse_autoencoder = language_model_sae_runner(LanguageModelSAERunnerConfig(**cfg))
+        # train SAE on hook_point
+        # cfg = base_cfg.copy()
+        # cfg["seed"] = seed
+        cfg["is_transcoder"] = False
+        sparse_autoencoder = language_model_sae_runner(LanguageModelSAERunnerConfig(**cfg))
+        move_last_checkpoint(f"checkpoints/64x_gelu_2l_{layer}/seed{seed}_ln2_normalized")
 
-    # train SAE on out_hook_point
-    cfg = base_cfg.copy()
-    cfg["seed"] = seed
-    cfg["is_transcoder"] = False
-    cfg["hook_point"] = cfg["out_hook_point"]
-    cfg["hook_point_layer"] = cfg["out_hook_point_layer"]
-    sparse_autoencoder = language_model_sae_runner(LanguageModelSAERunnerConfig(**cfg))
+        # train SAE on out_hook_point
+        # cfg = base_cfg.copy()
+        # cfg["seed"] = seed
+        # cfg["is_transcoder"] = False
+        cfg["hook_point"] = cfg["out_hook_point"]
+        cfg["hook_point_layer"] = cfg["out_hook_point_layer"]
+        sparse_autoencoder = language_model_sae_runner(LanguageModelSAERunnerConfig(**cfg))
+        move_last_checkpoint(f"checkpoints/64x_gelu_2l_{layer}/seed{seed}_mlp_out")
